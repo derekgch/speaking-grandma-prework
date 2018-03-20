78b99916f01612a94a61d157df4a30a5445b0b7b
derek chen <derek.g.chen@gmail.com>
2018-03-20 16:27:15 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,9 +13,9 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma (words)
-  if(word != word.uppercase)
+  if(words != words.uppercase)
     return 'HUH?! SPEAK UP, SONNY!'
-  elsif(word == 'I LOVE YOU GRANDMA')
+  elsif(words == 'I LOVE YOU GRANDMA!')
     return 'I LOVE YOU TOO PUMPKIN!'
   else
     return 'NO, NOT SINCE 1938!'
